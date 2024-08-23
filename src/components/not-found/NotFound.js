a277e153aa62525f12c726420e7c0c041ae1d05f
Mohan Singh Mahecha <mohansinghmahecha2000@gmail.com>
2024-08-23 23:37:05 +0530

    seo 0.1

--- src/components/not-found/NotFound.js
+++ src/components/not-found/NotFound.js
@@ -1,8 +1,14 @@
+import React from "react";
+import { Helmet } from "react-helmet";
 import img404 from "../../assets/404.jpg";
+
 export default function NotFound() {
   return (
     <div className="w-full block relative flex justify-center">
-      <img src={img404} alt={img404} width={512} height={700}/>
+      <Helmet>
+        <title>404 Page Not Found</title>
+      </Helmet>
+      <img src={img404} alt="404 Page Not Found" width={512} height={700} />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot, hydrateRoot } from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
+const rootElement = document.getElementById("root");
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+if (rootElement.hasChildNodes()) {
+  // If there's existing server-rendered content, use hydrateRoot
+  hydrateRoot(
+    rootElement,
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  // If no server-rendered content, use createRoot
+  createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
 
 reportWebVitals();
